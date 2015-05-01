9ecc51ee4bc0de901d54695e84902d62ebb1be8c
ftaran <farhad86@gmail.com>
2015-05-01 20:11:11 +0100

    angular added

--- Todos/App_Start/BundleConfig.cs
+++ Todos/App_Start/BundleConfig.cs
@@ -11,6 +11,12 @@ namespace Todos
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular/angular.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angularApp")
+            .IncludeDirectory("~/AngularApp", "*.js", true));
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
