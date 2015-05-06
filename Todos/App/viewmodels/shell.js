9e9fada11f52500f245a348d79e2cfc334ed63bd
Farhad Taran <farhad.taran@reedonline.co.uk>
2015-05-06 16:02:40 +0100

    welcome page added

--- Todos/App/viewmodels/shell.js
+++ Todos/App/viewmodels/shell.js
@@ -8,6 +8,7 @@
         },
         activate: function () {
             router.map([
+                { route: '', title: 'Welcome', moduleId: 'viewmodels/welcome', nav: true },
                 { route: 'tasks', moduleId: 'viewmodels/tasks', nav: true },
                 { route: 'completed', moduleId: 'viewmodels/completed', nav: true },
                 { route: 'logs', moduleId: 'viewmodels/logs', nav: true }
---
+++ Todos/App/viewmodels/tasks.js
@@ -0,0 +1,37 @@
+﻿define(['plugins/http', 'durandal/app', 'knockout'], function (http, app, ko) {
+
+    var logs = ko.observableArray();
+    var tasks = ko.observableArray();
+    var taskName = ko.observable('');
+
+    removeTask = function (task) {
+        tasks.remove(task);
+        log('Removed task: ' + this.taskName);
+    };
+
+    addTask = function () {
+        if (taskName().length != 0) {
+            tasks.push({ taskName: taskName(), done: ko.observable(false) });
+            log('Added new task: ' + taskName);
+            taskName('');
+        }
+    };
+
+    completeTask = function (task) {
+        task.done(true);
+        log("completed task: " + task.taskName)
+    }
+
+    log = function (message) {
+        logs.push(message)
+    };
+
+    // Return public methods
+    return {
+        taskName: taskName,
+        logs: logs,
+        tasks: tasks,
+        addTask: addTask,
+        removeTask: removeTask,
+    }
+});
